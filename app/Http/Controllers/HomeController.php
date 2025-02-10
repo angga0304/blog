bed6be5056df23eb1bb540f8044774915f8276b7
angga <angga0304@gmail.com>
2025-02-10 15:19:20 +0700

    add filter at frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,20 @@ use App\Models\Post;
 
 class HomeController extends Controller
 {
-    public function home() {
-        $posts = Post::where('active', TRUE)->get();
-        return view('homepage', compact('posts'));
+    public function home(Request $request) {
+        $param = $request->all();
+        $postsQuery = Post::where('active', TRUE);
+        if(!empty($param['search'])) {
+            $postsQuery->where('title', 'LIKE', '%'.$param['search'].'%');
+        }
+        if($param['order'] == 'oldest') {
+            $postsQuery->orderBy('created_at', 'asc');
+        }
+        else {
+            $postsQuery->orderBy('created_at', 'desc');
+        }
+        $posts = $postsQuery->get();
+        return view('homepage', compact('posts', 'param'));
     }
 
     
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,22 @@
     </x-slot>
 
     <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex">
+            <form class="max-w-sm" id="filter">
+                <div class="flex justify-start px-3">
+                    <label for="order" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Order by:</label>
+                    <select name="order" onchange="filter()" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option value="latest">latest</option>
+                        <option @if($param['order'] == 'oldest') selected @endif value="oldest">oldest</option>
+                    </select>
+                </div>
+                
+                <div class="flex justify-end px-3">
+                    <label for="search" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Search:</label>
+                    <input type="text" value="{{ $param['search'] }}" name="search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="search a title">
+                </div>
+            </form>
+        </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
             @foreach($posts as $post)
@@ -25,3 +41,8 @@
         </div>
     </div>
 </x-app-layout>
+<script>
+    function filter() {
+        document.getElementById('filter').submit();
+    }
+</script>
